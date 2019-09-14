cc8f2ca807c40b8742cb03a721fad4ffc3f497e2
Reid Hansell <reidhansell@gmail.com>
2019-09-13 20:59:04 -0600

    Minor fixes to deploy

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use("/api/users", require("./routes/users"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("react/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "react", "build", "index.html"));
   });
 }
 
