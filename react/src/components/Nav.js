a70f8be08141abdb56e631b35cb1f7803d60e329
Reid Hansell <reidhansell@gmail.com>
2019-09-15 14:57:28 -0600

    Navbar fixes

--- react/src/components/Nav.js
+++ react/src/components/Nav.js
@@ -6,84 +6,93 @@ const Nav = props => {
   const [open, setOpen] = useState(props.open || false);
   const { responseGoogle, isAuthenticated, logout } = props;
   return (
-    <>
-      <nav
-        className="navbar"
-        role="navigation"
-        aria-label="main navigation"
-        style={{ backgroundColor: "darkgray" }}
-      >
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item" onClick={() => setOpen(false)}>
-            <h3 className="title is-3 clickable">Stonks</h3>
-          </Link>
+    <nav
+      className="navbar"
+      role="navigation"
+      aria-label="main navigation"
+      style={{ backgroundColor: "darkgray" }}
+    >
+      {isAuthenticated ? (
+        <>
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item" onClick={() => setOpen(false)}>
+              <h3 className="title is-3 clickable">Stonks</h3>
+            </Link>
 
-          <button
-            style={{ background: "none", border: "none", outline: "none" }}
-            className="navbar-burger burger clickable"
-            onClick={() => setOpen(!open)}
+            <button
+              style={{
+                background: "none",
+                border: "none",
+                outline: "none"
+              }}
+              className="navbar-burger burger clickable"
+              onClick={() => setOpen(!open)}
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </button>
+          </div>
+          <div
+            id="navbarBasicExample"
+            className={"navbar-menu " + (open ? "is-active" : null)}
+            style={{ backgroundColor: "darkgray" }}
           >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </button>
-        </div>
-
-        <div
-          id="navbarBasicExample"
-          className={"navbar-menu " + (open ? "is-active" : null)}
-          style={{ backgroundColor: "darkgray" }}
-        >
-          <div className="navbar-end">
-            {isAuthenticated ? (
-              <>
-                <div className="navbar-item">
-                  <Link to="/" onClick={() => setOpen(false)}>
-                    <h5 className="subtitle is-5 clickable">Watchlist</h5>
-                  </Link>
-                </div>
-                <div className="navbar-item">
-                  <Link to="/profile" onClick={() => setOpen(false)}>
-                    <h5 className="subtitle is-5 clickable">Profile</h5>
-                  </Link>
-                </div>
-              </>
-            ) : null}
-            <div className="navbar-item">
-              <Link to="/about" onClick={() => setOpen(false)}>
-                <h5 className="subtitle is-5 clickable">About</h5>
-              </Link>
-            </div>
-            <div className="navbar-item">
-              <Link to="/contact" onClick={() => setOpen(false)}>
-                <h5 className="subtitle is-5 clickable">Contact</h5>
-              </Link>
-            </div>
-            <div className="navbar-item">
-              {isAuthenticated ? (
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <Link to="/" onClick={() => setOpen(false)}>
+                  <h5 className="subtitle is-5 clickable">Watchlist</h5>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/profile" onClick={() => setOpen(false)}>
+                  <h5 className="subtitle is-5 clickable">Profile</h5>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/about" onClick={() => setOpen(false)}>
+                  <h5 className="subtitle is-5 clickable">About</h5>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/contact" onClick={() => setOpen(false)}>
+                  <h5 className="subtitle is-5 clickable">Contact</h5>
+                </Link>
+              </div>
+              <div className="navbar-item">
                 <span onClick={() => setOpen(false)}>
                   <GoogleLogout
                     clientId="158562636348-ah58g7s1o64c16h1alsguklp5595r4uo.apps.googleusercontent.com"
                     buttonText="Logout"
                     onLogoutSuccess={() => logout()}
-                  ></GoogleLogout>
-                </span>
-              ) : (
-                <span onClick={() => setOpen(false)}>
-                  <GoogleLogin
-                    clientId="158562636348-ah58g7s1o64c16h1alsguklp5595r4uo.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={"single_host_origin"}
                   />
                 </span>
-              )}
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </>
+        </>
+      ) : (
+        <>
+          <div className="navbar-brand" style={{ width: "100%" }}>
+            <Link to="/" className="navbar-item" onClick={() => setOpen(false)}>
+              <h3 className="title is-3 clickable">Stonks</h3>
+            </Link>
+
+            <div className="navbar-item" style={{ marginLeft: "auto" }}>
+              <span onClick={() => setOpen(false)}>
+                <GoogleLogin
+                  clientId="158562636348-ah58g7s1o64c16h1alsguklp5595r4uo.apps.googleusercontent.com"
+                  buttonText="Login"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy={"single_host_origin"}
+                />
+              </span>
+            </div>
+          </div>
+        </>
+      )}
+    </nav>
   );
 };
 
